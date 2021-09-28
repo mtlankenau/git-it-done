e5a414aa8eee16e102d22f3bc20031160d483aad
mtlankenau <mtlankenau@gmail.com>
2021-09-28 16:04:10 -0400

    successfully able to display username and all their repos & issues status to page, and handled several errors using if statements and .catch method of fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,77 @@
+// selector variables for form and username
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+// after clicking "Get User", function gets username input and passes as argument to getUserRepos function call
+let formSubmitHandler = function(event) {
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+// uses username input to fetch correct github API endpoint (repos) and passes repos and username as arguments to displayRepos function call  
 let getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("error: GitHub User Not Found");
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect to GitHub");
         });
-    });
 };
 
-getUserRepos("mtlankenau");
+// displays the github repos of the particular username to the page
+let displayRepos = function(repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found for this user.";
+        return;
+    };
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name "i.e. mtlankenau/git-it-done"
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex row justify-space-between align-center";
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        // append to container
+        repoEl.appendChild(titleEl);
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        //check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        };
+        // append to container
+        repoEl.appendChild(statusEl);
+        // append container to DOM
+        repoContainerEl.appendChild(repoEl);
+    };
+};
+
+// event listener for submitting form by clicking "Get User" button
+userFormEl.addEventListener("submit", formSubmitHandler);
