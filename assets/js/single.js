8ee1710873c155f8b3d9669122be87629525dbdc
mtlankenau <mtlankenau@gmail.com>
2021-09-30 17:19:05 -0400

    dynamically added extra message with link to github repo if more than 30 issues exist

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 function getRepoIssues(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,6 +7,10 @@ function getRepoIssues(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request!");
@@ -47,4 +52,16 @@ function displayIssues(issues) {
     };
 };
 
-getRepoIssues("mtlankenau/git-it-done");
+function displayWarning(repo) {
+    // add text content to limit-warning div
+    limitWarningEl.textContent = "To see more than 30 issues, visit "
+    // <a> tag with link to github repo URL
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // append <a> tag to limit-warning div
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
